c8bdbbe7eaa5243871b36e3a906713b89ab6129a
ludyxgundyr123 <ludyxgundyr123@gmail.com>
2024-11-10 18:12:35 +0800

    added comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,25 +2,25 @@
 
 
 const sumAll = function(a,b) {
-     let result = [];
-     let sum = 0;
+     let result = [];// empty array
+     let sum = 0;// variable to hold total sum of numbers in array
      
-     if (Number.isInteger(a) && Number.isInteger(b) && a <b && a >=1 && b>=1){
-        for(let i = a ; i<=b ; i++){
-            result.push(i);
+     if (Number.isInteger(a) && Number.isInteger(b) && a <b && a >=1 && b>=1){//if a& b is an integer and a is less than b and a & b is more than or equal to 1, run
+        for(let i = a ; i<=b ; i++){// loop from the range a to b
+            result.push(i);// push the numbers from range a to b to an array called result
         }
            
         
-            for(let j = 0 ; j < result.length ; j++){
+            for(let j = 0 ; j < result.length ; j++){//loop through the length of the array result
             
                 
-                sum += result[j];
+                sum += result[j];// sum all numbers in the array result
     
             }
-            return sum ;
+            return sum ;//return the value of the added nmbers
         
-     }else if (a > b){
-        for(let i = b ; i<=a ; i++){
+     }else if (a > b){//if a is more than b(so it works with large numbers first)
+        for(let i = b ; i<=a ; i++){// changed position of a to b 
             result.push(i);
         }
            
