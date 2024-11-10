c15e14981ea3b94092b07d926d0c60e07a2556cd
ludyxgundyr123 <ludyxgundyr123@gmail.com>
2024-11-10 17:18:08 +0800

    completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+
+const leapYears = function(years) {
+    if (years % 4 == 0 && years % 100 !== 0 || years % 400 == 0 ){
+        return true;
+    
+    }else {
+        return false;
+    }
 
 };
 
