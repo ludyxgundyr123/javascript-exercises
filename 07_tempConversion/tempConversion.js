136c2f8adf38739764dd45a742874649802c0573
ludyxgundyr123 <ludyxgundyr123@gmail.com>
2024-11-10 17:40:18 +0800

    completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celcius = (farenheit - 32) * 5/9 // converst farenheit to celcius
+
+  return temperature = Math.round(celcius * 10)/10 ;//return celcius that has been rounded up to 1 decimal point
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = celcius * (9/5) + 32 ;// converts celcius to fahrenheit
+  return temperature = Math.round(fahrenheit *10)/10;
 };
 
 // Do not edit below this line
