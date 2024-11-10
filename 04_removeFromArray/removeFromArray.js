c62ad230411c07ac5a43c202d38743e16b7f3b43
ludyxgundyr123 <ludyxgundyr123@gmail.com>
2024-11-10 18:16:15 +0800

    added comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,8 +3,8 @@
 const removeFromArray = function(arr,val1,val2,val3,val4){
     for (let j = 1 ; j < 20 ; j++){  //loop the loop multiple times until all item of array that is = to valuetoremove is gone
      for (let i = 0; i <arr.length ; i++){ //  loop the length of the array 
-        if(arr[i]=== val1 || arr[i]=== val2 || arr[i]=== val3 || arr[i]=== val4){ 
-        arr.splice(i,1);
+        if(arr[i]=== val1 || arr[i]=== val2 || arr[i]=== val3 || arr[i]=== val4){ //if item in i is equal to val1/val2/val3/val4
+        arr.splice(i,1);//remove item from i
     }
      }
  }
