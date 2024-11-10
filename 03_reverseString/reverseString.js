a13d7e4c67c0746c29f9f4fe9059372f078e6fb2
ludyxgundyr123 <ludyxgundyr123@gmail.com>
2024-11-10 17:16:01 +0800

    completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+// work for all tests (just change value in text)
+const reverseString = function(string) {
+    let text =string;
+    const newText =Array.from(text);// put text in an array
+    return newText.reverse().join("");// reverse element in array and return it without brackets
 
+    
 };
 
 // Do not edit below this line
