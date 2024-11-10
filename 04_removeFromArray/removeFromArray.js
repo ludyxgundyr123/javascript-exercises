dfe0fc82cdf42b736a41ab4214f353628406f966
ludyxgundyr123 <ludyxgundyr123@gmail.com>
2024-11-10 17:16:38 +0800

    completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
 
+
+const removeFromArray = function(arr,val1,val2,val3,val4){
+    for (let j = 1 ; j < 20 ; j++){  //loop the loop multiple times until all item of array that is = to valuetoremove is gone
+     for (let i = 0; i <arr.length ; i++){ //  loop the length of the array 
+        if(arr[i]=== val1 || arr[i]=== val2 || arr[i]=== val3 || arr[i]=== val4){ 
+        arr.splice(i,1);
+    }
+     }
+ }
+      return arr;
+  }
 // Do not edit below this line
 module.exports = removeFromArray;
