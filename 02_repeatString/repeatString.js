d9f8b76b099577bfc55bb2d820ee1857f22f4675
ludyxgundyr123 <ludyxgundyr123@gmail.com>
2024-11-10 17:15:17 +0800

    completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(str,number) {
+    
+       let name = str;
+       if (number <= -1){
+        return 'ERROR';
+       }else if (number == 0)  {
+        return '';
+        
+         
+       }else {
+        for (let i = 0; i < number -1 ; ++i ){
+            name += str;
+        }
+        return name;
+       }
+       
+       
+       
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
