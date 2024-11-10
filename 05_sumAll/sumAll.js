55fe1268fe1daa1051bb575c614ed280653f9993
ludyxgundyr123 <ludyxgundyr123@gmail.com>
2024-11-10 17:17:18 +0800

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,57 @@
-const sumAll = function() {
 
-};
+
+
+const sumAll = function(a,b) {
+     let result = [];
+     let sum = 0;
+     
+     if (Number.isInteger(a) && Number.isInteger(b) && a <b && a >=1 && b>=1){
+        for(let i = a ; i<=b ; i++){
+            result.push(i);
+        }
+           
+        
+            for(let j = 0 ; j < result.length ; j++){
+            
+                
+                sum += result[j];
+    
+            }
+            return sum ;
+        
+     }else if (a > b){
+        for(let i = b ; i<=a ; i++){
+            result.push(i);
+        }
+           
+        
+            for(let j = 0 ; j < result.length ; j++){
+            
+                
+                sum += result[j];
+    
+            }
+            return sum ;
+          
+        
+        
+        
+
+     }else  {
+        return "ERROR";
+        
+        
+     }
+     
+     
+ 
+ };
+ 
+          
+    
+    
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
